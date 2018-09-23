f3d8ab05a928f01e1f30d59b663a8e3d61f1bd7b
Hitesh Lilhare <hitesh.lilhare@gmail.com>
2018-09-24 00:22:54 +0530

    Added for keeping Card Details like CPLC,ATR,Card data, Card Capabilities etc.

---
+++ src/main/java/com/github/hiteshlilhare/jcplaystore/CardDetails.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.hiteshlilhare.jcplaystore;
+
+import javax.smartcardio.ATR;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pro.javacard.gp.GPData;
+
+/**
+ *
+ * @author Hitesh
+ */
+public class CardDetails {
+    private static final Logger logger = LoggerFactory.getLogger(CardDetails.class);
+    /**
+     * Answer To Reset.
+     */
+    private ATR atr;
+    /**
+     * Card Production Life Cycle Data (CPLC data)
+     */
+    private GPData.CPLC cplc;
+    //When issuer issues a card, issuers should set the IIN (issuer identification number) 
+    //and CIN (card image number) in the card manager.
+    /**
+     * Issuer Identification Number
+     */
+    private byte[] inn;
+    /**
+     * Card Image Number
+     */
+    private byte[] cin;
+    
+    private byte[] cardData;
+    
+    private byte[] cardCapabilities;
+    
+    private byte[] keyInfo;
+
+    public ATR getAtr() {
+        return atr;
+    }
+
+    public void setAtr(ATR atr) {
+        this.atr = atr;
+    }
+
+    public GPData.CPLC getCplc() {
+        return cplc;
+    }
+
+    public void setCplc(GPData.CPLC cplc) {
+        this.cplc = cplc;
+    }
+
+    public byte[] getInn() {
+        return inn;
+    }
+
+    public void setInn(byte[] inn) {
+        this.inn = inn;
+    }
+
+    public byte[] getCin() {
+        return cin;
+    }
+
+    public void setCin(byte[] cin) {
+        this.cin = cin;
+    }
+
+    public byte[] getCardData() {
+        return cardData;
+    }
+
+    public void setCardData(byte[] cardData) {
+        this.cardData = cardData;
+    }
+
+    public byte[] getCardCapabilities() {
+        return cardCapabilities;
+    }
+
+    public void setCardCapabilities(byte[] cardCapabilities) {
+        this.cardCapabilities = cardCapabilities;
+    }
+
+    public byte[] getKeyInfo() {
+        return keyInfo;
+    }
+
+    public void setKeyInfo(byte[] keyInfo) {
+        this.keyInfo = keyInfo;
+    }
+    
+}
