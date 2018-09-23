192fcfb25a03c1c74ce636c8df0cb4d6b8ddfa68
Hitesh Lilhare <hitesh.lilhare@gmail.com>
2018-09-24 00:21:39 +0530

    Added as Enum class for card_app database table

---
+++ src/main/java/com/github/hiteshlilhare/jcplaystore/CardAppTableFields.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.hiteshlilhare.jcplaystore;
+
+/**
+ *
+ * @author Hitesh
+ */
+public enum CardAppTableFields {
+    ICFabricator,
+    ICSerialNumber,
+    ICType,
+    AID,
+    Version,
+    Description
+}
