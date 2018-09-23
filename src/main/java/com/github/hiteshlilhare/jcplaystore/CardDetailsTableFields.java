304518b16bf95d16bab3c5eec3fa1a41bc06be8f
Hitesh Lilhare <hitesh.lilhare@gmail.com>
2018-09-24 00:24:04 +0530

    Added as Enum Class for fields of card_details database table

---
+++ src/main/java/com/github/hiteshlilhare/jcplaystore/CardDetailsTableFields.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.hiteshlilhare.jcplaystore;
+
+/**
+ * Inspired by GPData.CPLC.Fields enum of GlobalPlatformPro
+ * @author Hitesh
+ */
+public enum CardDetailsTableFields {
+    ICFabricator,
+    ICType,
+    OperatingSystemID,
+    OperatingSystemReleaseDate,
+    OperatingSystemReleaseLevel,
+    ICFabricationDate,
+    ICSerialNumber,
+    ICBatchIdentifier,
+    ICModuleFabricator,
+    ICModulePackagingDate,
+    ICCManufacturer,
+    ICEmbeddingDate,
+    ICPrePersonalizer,
+    ICPrePersonalizationEquipmentDate,
+    ICPrePersonalizationEquipmentID,
+    ICPersonalizer,
+    ICPersonalizationDate,
+    ICPersonalizationEquipmentID,
+    ATR,
+    INN,
+    CIN,
+    CardData,
+    CardCapability,
+    KeyInfo
+}
